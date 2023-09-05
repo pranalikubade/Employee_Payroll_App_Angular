e76395ce26dfa047ae539ebb7374173d54a72c49
pranalikubade <pranalikubade2000@gmail.com>
2023-09-05 11:42:38 +0530

    Added Header and Home Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEmployeeeComponent } from './component/add-employeee/add-employeee.component';
+import { HomeComponent } from './component/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'add',component:AddEmployeeeComponent },
+ {path:'',component:AddEmployeeeComponent },
+  {path:'home',component:HomeComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/component/add-employeee/add-employeee.component.ts
+++ src/app/component/add-employeee/add-employeee.component.ts
@@ -1,99 +1,75 @@
 
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatCheckboxChange } from '@angular/material/checkbox';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Employee } from 'src/app/model/employee.model';
+
 
 @Component({
   selector: 'app-add-employeee',
   templateUrl: './add-employeee.component.html',
   styleUrls: ['./add-employeee.component.css']
 })
-export class AddEmployeeComponent implements OnInit{
-//public employee: Employee = new Employee();
+export class AddEmployeeeComponent implements OnInit{
+  public employee:Employee=new Employee();
+  employeeFormGroup: FormGroup<any>;
 
-employeeForm!: FormGroup
+
   departments: Array<any> = [
-    {
-      id: 1,
-      name: "HR",
-      value: "HR",
-      checked: false
-    },
-    {
-      id: 2,
-      name: "Sales",
-      value: "Sales",
-      checked: false 
-    },
-    {
-      id: 3,
-      name: "Finance",
-      value: "Finance",
-      checked: false  
-    },
-    {
-      id: 4,
-      name: "Engineer",
-      value: "Engineer",
-      checked: false
-    },
-    {
-      id: 5,
-      name: "Other",
-      value: "Other",
-      checked: false 
-    }
+    {id: 1, name: "HR",value: "HR", checked: false},
+    {id: 2, name: "Sales",value: "Sales", checked: false },
+    {id: 3,name: "Finance",value: "Finance",checked: false },
+    {id: 4, name: "Engineer",value: "Engineer",checked: false},
+    {id: 5,name: "Other",value: "Other",checked: false  }
   ]
+ constructor(private formBuilder: FormBuilder, 
+    private router: Router,
+   private activatedRoute: ActivatedRoute,
+    private snackBar: MatSnackBar) {
+this.employeeFormGroup = this.formBuilder.group({
+name: new FormControl('', [Validators.required, Validators.pattern("^[A-Z][a-zA-Z\\s]{2,}$")]),
+profilePic: new FormControl('', [Validators.required]),
+gender: new FormControl('', [Validators.required]),
+department: this.formBuilder.array([], [Validators.required]),
+salary: new FormControl('', [Validators.required]),
+startDate: new FormControl('', [Validators.required]),
+note: new FormControl('', [Validators.required]) 
+})
+}
+ngOnInit(): void {
+  throw new Error('Method not implemented.');
+}
+
+
   salary: number = 400000;
-  salaryForm: any;
+  updateSetting(event:any) {
+    this.salary = event.value;
+  }
+
   formatLabel(value: number) {
     if (value >= 1000) {
       return Math.round(value / 1000) + 'k';
     }
     return value;
   }
+  onCheckboxChange(event: MatCheckboxChange) {
+    const department: FormArray = this.employeeFormGroup.get('department') as FormArray;
 
-  updateSetting(event: Event) {
-    this.salary = (event.target as HTMLInputElement).valueAsNumber;
-    console.log('Selected Salary:', this.salary);
-    this.salaryForm.get('salary')?.setValue(this.salary);
+    if (event.checked) {
+      department.push(new FormControl(event.source.value));
+    } else {
+      const index = department.controls.findIndex(x => x.value === event.source.value);
+      department.removeAt(index);
+    }
   }
-
-  employeeFormGroup: FormGroup;
-
-  constructor(private formBuilder: FormBuilder ) {
-    this.employeeFormGroup = this.formBuilder.group({
-      name: '',
-      profileimage:'',
-      gender:'',
-      department:'',
-      salary:'',
-      date:'',
-      notes:''
-
-    });
-  }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-
   onSubmit() {
     const dataString = JSON.stringify(this.employeeFormGroup.value);
     localStorage.setItem('formData', dataString);
     this.employeeFormGroup.reset();
+  console .log (dataString)
   }
 
-  onDepartmentChange(event: any) {
-    const departmentValue = event.value
-    const SelectedDepartment = event.checked
-    const departmentArray : FormArray =this.employeeForm.get('department') as FormArray;
-
-    if (SelectedDepartment) {
-      departmentArray.push(new FormControl(departmentValue));
-    } else {
-      const index = departmentArray.controls.findIndex(x => x.value ===departmentValue);
-      departmentArray.removeAt(index);
-    }
-  }
 
 }
